54caaa6a8bd6896205da5ec7c0196f37528e4512
Tetiana <tvts1008@gmail.com>
2024-10-19 20:31:44 +0300

    Change1 in file1.1.py

--- folder1/file1.1.py
+++ folder1/file1.1.py
@@ -1,8 +1,8 @@
 #exe.3 Програма для визначення числа
-n=int(input("Enter number: "))
+n=int(input("Enter number: ")) # Введіть число
 if n == 0:
-    print("Number is equal to zero")
+    print("Number is equal to zero") # Число дорівнює нулю
 elif n > 0: # Позитивне число
-    print("Number is positive")
+    print("Number is positive") # Позитивне число
 else: # Від'ємне число
-    print("Number is negative")
+    print("Number is negative") # Негативне число
