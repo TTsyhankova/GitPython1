e45789e8f5cb47918f364eb9d2fe68d4b550bbae
Tetiana <tvts1008@gmail.com>
2024-10-19 20:46:53 +0300

    Change1 in file2.1.py

--- folder2/file2.1.py
+++ folder2/file2.1.py
@@ -1,6 +1,6 @@
 #exe.5 Програма порівняння двох чисел
-n1=int(input("Enter number1: "))
-n2=int(input("Enter number2: "))
+n1=int(input("Enter number1: ")) # Введіть перше число
+n2=int(input("Enter number2: ")) # Введіть друге число
 if n1 < n2:
     print(n1, n2)
 elif n1 > n2:
